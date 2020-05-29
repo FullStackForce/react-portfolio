e388aa03333bac1ac266ec9e61badfbb7539cf91
Full Stack <fullstack@fullstacks-MacBook-Pro.local>
2020-05-29 09:48:07 -0400

    update port and package

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'build');
-app.use(express.static(publicPath));
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("build"));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(port, () => {
-   console.log(`Server is up on port ${port}!`);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
