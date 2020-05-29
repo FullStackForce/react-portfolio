1c720cb3a32147376a293f895d72ba104e0e7b6c
Full Stack <fullstack@fullstacks-MacBook-Pro.local>
2020-05-29 00:57:04 -0400

    update

---
+++ build/precache-manifest.261fb0f9f9ac37e121d6c4fb7a840948.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a23614c17b6e3542d096225bc7cb6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3c643af6cc96edc93bd",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "5c0b42d5df90d604423f",
+    "url": "/static/js/2.56adf9a1.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.56adf9a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3c643af6cc96edc93bd",
+    "url": "/static/js/main.4a2fac7d.chunk.js"
+  },
+  {
+    "revision": "97ad9e083897a8286841",
+    "url": "/static/js/runtime-main.d7d8550f.js"
+  }
+]);
