5b6f1bfe61cb58a175f80e45e42bb30e6bf7a0ba
Full Stack <7883863+FullStackForce@users.noreply.github.com>
2020-07-28 12:12:00 -0400

    update,

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'public');
-app.use(express.static(publicPath));
-
-app.get('/*', function (req, res) {
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(port, () => {
-   console.log(`Server is up on port ${port}!`);
-});
+app.listen(3000);
